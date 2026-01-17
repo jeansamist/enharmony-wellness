b9d69018cd184f93843c2a99072de8375ac21320
BAHA Ephraim <jeansamist@gmail.com>
2026-01-17 21:47:27 +0100

    feat: simplify post publishing by removing PostEditor and directly publishing posts

--- app/app/posts/[slug]/publish/page.tsx
+++ app/app/posts/[slug]/publish/page.tsx
@@ -1,6 +1,5 @@
-import { PostEditor } from "@/components/post-editor";
-import { getCategories, getPostBySlug } from "@/services/post.services";
-import { getReviewers } from "@/services/user.services";
+import { getPostBySlug, publishPost } from "@/services/post.services";
+import { redirect } from "next/navigation";
 
 export default async function page({
   params,
@@ -9,26 +8,9 @@ export default async function page({
 }) {
   const { slug } = await params;
   const post = await getPostBySlug(slug);
-  const categories = await getCategories();
-  const reviewers = await getReviewers();
   if (!post) {
     return <div>Post not found</div>;
   }
-  return (
-    <div>
-      <PostEditor
-        categories={categories}
-        reviewers={reviewers}
-        title={post.title}
-        content={post.content}
-        description={post.description}
-        default_cover={post.cover}
-        default_video_url={post.video_url || undefined}
-        default_type={post.type}
-        default_slug={post.slug}
-        default_category={post.category}
-        default_reviewer={post.reviews[0]?.user || undefined}
-      />
-    </div>
-  );
+  await publishPost(Number(post.id));
+  redirect(`/articles/${post.slug}`);
 }
